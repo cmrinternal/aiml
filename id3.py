d2afa04d07b207889676d11e2dfd8d40d9bd613b
pass <phenixfire00@gmail.com>
2025-11-19 21:15:31 +0530

    Update id3.py

--- id3.py
+++ id3.py
@@ -2,9 +2,7 @@ import pandas as pd
 from collections import Counter
 import math
 
-# -----------------------------
-# ENTROPY FUNCTIONS
-# -----------------------------
+
 def entropy(probs):
     return sum(-p * math.log(p, 2) for p in probs if p > 0)
 
@@ -14,9 +12,7 @@ def entropy_of_list(values_list):
     probs = [c / total for c in count.values()]
     return entropy(probs)
 
-# -----------------------------
-# INFORMATION GAIN
-# -----------------------------
+
 def information_gain(df, split_attribute_name, target_attribute_name):
     df_split = df.groupby(split_attribute_name)
     total_len = len(df)
@@ -31,28 +27,26 @@ def information_gain(df, split_attribute_name, target_attribute_name):
     original_entropy = entropy_of_list(df[target_attribute_name])
     return original_entropy - weighted_entropy
 
-# -----------------------------
-# ID3 DECISION TREE
-# -----------------------------
+
 def id3DT(df, target_attribute_name, attribute_names, default_class=None):
     cnt = Counter(df[target_attribute_name])
 
-    # If only one class remaining
+   
     if len(cnt) == 1:
         return next(iter(cnt))
 
-    # If no data or attributes left
+   
     if df.empty or not attribute_names:
         return default_class
 
-    # Default class = majority class
+   
     default_class = max(cnt, key=cnt.get)
 
-    # Calculate information gain for each attribute
+  
     gains = [information_gain(df, att, target_attribute_name) for att in attribute_names]
     best_attr = attribute_names[gains.index(max(gains))]
 
-    # Build the tree
+  
     tree = {best_attr: {}}
     remaining_attrs = [a for a in attribute_names if a != best_attr]
 
@@ -62,9 +56,7 @@ def id3DT(df, target_attribute_name, attribute_names, default_class=None):
 
     return tree
 
-# -----------------------------
-# UPDATED DATASET (windy → weak/strong)
-# -----------------------------
+
 data = {
     'outlook': ['sunny','sunny','overcast','rain','rain','rain','overcast','sunny','sunny','rain','sunny','overcast','overcast','rain'],
     'temperature': ['hot','hot','hot','mild','cool','cool','cool','mild','cool','mild','mild','mild','hot','mild'],
@@ -74,10 +66,6 @@ data = {
 }
 
 df = pd.DataFrame(data)
-
-# -----------------------------
-# RUN ID3
-# -----------------------------
 attribute_names = ['outlook', 'temperature', 'humidity', 'windy']
 decision_tree = id3DT(df, 'play', attribute_names)
 
